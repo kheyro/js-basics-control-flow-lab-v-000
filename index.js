74c3f0c044743e6c880d54ab74ced32dea6e1691
Denis Atkesone <denis.atkesone@gmail.com>
2018-03-15 09:26:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Write your code in this file!
 function scuberGreetingForFeet(feet) {
-  if feet < 400
+  if feet <= 400
     'This one is on me!'
   else if feet > 2000
     'I will gladly take your thirty bucks.'
