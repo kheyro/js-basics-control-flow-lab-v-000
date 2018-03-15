a293acb3cd9678f7aad325ae49c72bc37fd9ed4b
Denis Atkesone <denis.atkesone@gmail.com>
2018-03-15 09:51:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,6 +13,16 @@ function ternaryCheckCity(city) {
   return (city === 'NYC') ? 'Ok, sounds good.' : 'No go.'
 }
 
-function switchOnCharmFromTip(tips) {
-  
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much. '
+      break;
+    case 'not as generous':
+      return 'Thank you.'
+      break;
+    default:
+      return 'Bye.'
+      break;
+  }
 }
